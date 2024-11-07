9edd36fcc92164ee80b2f0b7508eee39111ba21e
kdy <sksl6134@gmail.com>
2024-11-07 16:54:47 +0900

    chapter 16

--- _ch15/config/config.go
+++ _ch15/config/config.go
@@ -4,13 +4,11 @@ import (
 	"github.com/caarlos0/env/v6"
 )
 
-// 환경 변수로부터 값을 읽어옴
 type Config struct {
 	Env  string `env:"TODO_ENV" envDefault:"dev"`
 	Port int    `env:"PORT" envDefault:"80"`
 }
 
-// New는 환경 변수로부터 설정값을 읽어와 Config 구조체를 생성하고 초기화하여 반환
 func New() (*Config, error) {
 	cfg := &Config{}
 	if err := env.Parse(cfg); err != nil {
