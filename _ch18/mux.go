4f69bf35f569fb9ed5c174fc51fd168faf4557ce
kdy <sksl6134@gmail.com>
2024-11-21 21:06:00 +0900

    ch18

---
+++ _ch18/mux.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/doyoung0013/go_todo_app/_ch18/handler"
+	"github.com/doyoung0013/go_todo_app/_ch18/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+)
+
+func NewMux() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	v := validator.New()
+
+	mux.Handle("/tasks", &handler.AddTask{Store: store.Tasks, Validator: v})
+
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
+	return mux
+}
